8e339bb700c5307dc7860364055eb70368a0453a
Viraj <vis68354@gmail.com>
2024-07-02 14:28:55 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,12 +7,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    // generate .vite/manifest.json in outDir
-    manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: '/path/to/main.js',
-    },
+    chunkSizeWarningLimit: 1600,
   },
 })
 
