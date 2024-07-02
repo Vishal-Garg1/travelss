ef421cee24a198484c4451082167364ede53a74d
Viraj <vis68354@gmail.com>
2024-07-02 18:47:53 +0530

    Some bug fixes

--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -1,8 +1,11 @@
-export default function Image({src,...rest}) {
-  src = src && src.includes('https://')
-    ? src
-    : 'http://localhost:4000/uploads/'+src;
-  return (
-    <img {...rest} src={src} alt={''} />
-  );
+export default function Image({ src, ...rest }) {
+  let imageUrl;
+
+  if (src && (src.startsWith('https://') || src.startsWith('http://'))) {
+    imageUrl = src;
+  } else {
+    imageUrl = 'http://localhost:4000/uploads/' + src;
+  }
+
+  return <img {...rest} src={imageUrl} alt={'error'} />;
 }
