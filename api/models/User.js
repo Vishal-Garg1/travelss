0877b93960f956840a44db56abea5386d71d9819
Vishal-Garg1 <140538906+Vishal-Garg1@users.noreply.github.com>
2024-07-05 13:18:26 +0530

    Update User.js

--- api/models/User.js
+++ api/models/User.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
-const UserSchema = new Schema({
+const UserSchema = new Schema(
+  {
   name: String,
   email: {type:String, unique:true},
   password: String,
-});
+  },
+  {
+    timestamps:true
+  }
+);
 
 const UserModel = mongoose.model('User', UserSchema);
 
